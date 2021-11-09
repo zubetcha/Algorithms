c5e41a72b0ad30bfe0a6146aa1cdc8155437defe
zubetcha <zuhye5@gmail.com>
2021-11-10 00:30:05 +0900

    Programmers Algorithms level1

---
+++ algorithms3.js
@@ -0,0 +1,43 @@
+
+// 문제 설명
+// 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+// 재한사항
+// s는 길이가 1 이상, 100이하인 스트링입니다.
+// 입출력 예
+// s	return
+// "abcde"	"c"
+// "qwer"	"we"
+
+// 문자열의 문자 개수 세기
+// 조건문
+// 문자의 개수가 짝수라면 (문자 개수/2)-1 번째와 (문자 개수/2 + 1) 번째의 문자 반환
+// 문자의 개수가 홀수라면 문자 개수/2의 반내림 정수 번째의 문자 반환
+
+function solution(text) {
+    let cnt = text.length;
+    let index = cnt/2;
+    if (cnt % 2 == 0) {
+      result = text.slice(index-1, index+1);
+    } else {
+      result = text.charAt(Math.floor(cnt/2));
+    }
+    return result;
+};
+
+function solution(text) {
+  let cnt = text.length;
+  let index = cnt/2;
+  if (cnt % 2 == 0) {
+    return text.slice(index-1, index+1);
+  } else {
+    return text.charAt(Math.floor(cnt/2));
+  }
+};
+
+
+function solution(text) {
+    let cnt = text.length;
+    let index = cnt/2;
+    return cnt % 2 == 0 ? text.slice(index-1, index+1) : text.charAt(Math.floor(cnt/2))
+}
---
+++ algorithms6.js
@@ -0,0 +1,38 @@
+// 문제 설명
+// 0부터 9까지의 숫자 중 일부가 들어있는 배열 numbers가 매개변수로 주어집니다. numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ numbers의 길이 ≤ 9
+// 0 ≤ numbers의 모든 수 ≤ 9
+// numbers의 모든 수는 서로 다릅니다.
+// 입출력 예
+// numbers	result
+// [1,2,3,4,6,7,8,0]	14
+// [5,8,4,0,6,7,9]	6
+// 입출력 예 설명
+// 입출력 예 #1
+
+// 5, 9가 numbers에 없으므로, 5 + 9 = 14를 return 해야 합니다.
+// 입출력 예 #2
+
+// 1, 2, 3이 numbers에 없으므로, 1 + 2 + 3 = 6을 return 해야 합니다.
+
+// 매개변수 numbers의 숫자 범위 선언이 필요한 지?? x
+// 뱐수 numList = numbers 배열에 없는 숫자들의 리스트
+// return numList의 각 숫자들의 합
+// for문
+
+
+function solution(numbers) {
+    let totalSum = 0;
+    for (let i = 0; i < 10; i++) {
+        totalSum += i;
+    }
+    let numbersSum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        numbersSum += numbers[i];
+    }
+    return totalSum - numbersSum;
+}
+
+// .reduce 메소드 사용해서도 작성 가능
