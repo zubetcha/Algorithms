e1c189e191201bb035bbb4381e07714e32e48209
zubetcha <zuhye5@gmail.com>
2021-11-11 00:33:09 +0900

    이상한 문자 만들기 수정

--- algorithms21.js
+++ algorithms21.js
@@ -15,16 +15,36 @@
 // 각 단어의 짝수번째 문자를 대문자로, 홀수번째 문자를 소문자로 바꾸면 
 // "TrY", "HeLlO", "WoRlD"입니다. 따라서 "TrY HeLlO WoRlD" 를 리턴합니다.
 
-// s.length >= 1
-// 문자열에서 공백을 포함해서 단어의 개수를 셀 때 단어 자체는 문자열의 짝수 인덱스 ex) 0, 2, 4...
-// 문자에서 치환해줘야 하는 인덱스도 짝수 인덱스 ex) 0, 2, 4 ...
+
+// split을 활용해서 공백을 기준으로 하는 단어 하나 하나를 요소로 가지는 배열 생성
+// map 메소드를 활용해서 배열의 각각의 요소들 (한 단어)를 돌면서 콜백함수 실행
+// 콜백함수는 각 요소 (문자열 한 단어) 의 i번째가 짝수 또는 홀수인지에 따라 i번째 문자를 대/소문자로 치환
+// 콜백함수 실행을 완료한 요소 (문자열 한 단어)를 다시 배열에 업데이트
+// 배열의 각 요소 (한 단어) 씩 탐색하며 콜백함수 실행을 마쳐서 만들어진 배열의 각 요소를
+// join 을 통해서 연결하여 문자열로 만들어 줌
+
 
 function solution(s) {
-    // 문자열의 길이는 공백을 무시하기 때문에 공백은 신경쓰지 않아도 됨
-    let result = '';
-    for (let i = 0; i < s.length; i++) {
-        // 짝수인 i번째 알파벳은 대문자로, 홀수인 i번째 알파벳은 소문자로 치환
-        i % 2 == 0 ?  result += s[i].toUpperCase() : result += s[i].toLowerCase();
-    }
-    return result;
-}
+    let wordList = s.split(" ");
+    return wordList.map(function(el) {
+        let word = '';
+        for (let i = 0; i < el.length; i++) {
+            i % 2 == 0 ? word += el[i].toUpperCase() : word += el[i].toLowerCase();
+        }
+        return word;
+    }).join(" ");
+};
+
+
+// s = "try hello world"
+// let wordList = s.split.(" ");
+// wordList = ['try', 'hello', 'world']
+// map 대소문자 치환 콜백함수
+// 1-1. 'try' => 'TrY'
+// 1-2. wordList = ['TrY', 'hello', 'world']
+// 2-1. 'hello' => 'HeLlO'
+// 2-2. wordList = ['TrY', 'HeLlO', 'world']
+// 3-1. 'world' => 'WoRlD'
+// 3-2. wordList = ['TrY', 'HeLlO', 'WoRlD']
+// wordList = ['TrY', 'HeLlO', 'WoRlD']
+// wordList.join(" ") = 'TrY HeLlO WoRlD'
