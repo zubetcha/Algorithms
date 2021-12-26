b9f82f2631781ac4048df1c2c058ccc5868abfcd
zubetcha <zuhye5@gmail.com>
2021-12-26 17:33:56 +0900

    프로그래머스 34, 35, 36 추가

--- programmers/Level-1/algorithms24.js
+++ programmers/Level-1/algorithms24.js
@@ -1,8 +1,8 @@
 // 프로그래머스 - 정수 내림차순으로 배치하기
 
 // 문제 설명
-// 함수 solution은 정수 n을 매개변수로 입력받습니다. 
-// n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 
+// 함수 solution은 정수 n을 매개변수로 입력받습니다.
+// n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요.
 // 예를들어 n이 118372면 873211을 리턴하면 됩니다.
 
 // 제한 조건
@@ -10,7 +10,6 @@
 // 입출력 예
 // n	return
 // 118372	873211
-
 
 // 정수 n을 문자열로 변환 String
 // 문자열의 문자를 하나씩 돌면서 nList에 각각의 문자열 요소로 push
@@ -22,35 +21,50 @@
 // 단, 문자가 섞여 있는 경우 NaN 반환
 
 function solution(n) {
-    let str = String(n);
-    let nList = [];
-    for (let i = 0; i < str.length; i++) {
-        nList.push(str[i]);
-    }
-    return parseInt(nList.sort(function(a, b) {return b - a;}).join(''));
+  let str = String(n)
+  let nList = []
+  for (let i = 0; i < str.length; i++) {
+    nList.push(str[i])
+  }
+  return parseInt(
+    nList
+      .sort(function (a, b) {
+        return b - a
+      })
+      .join('')
+  )
 }
 
 function solution(n) {
-    let str = String(n);
-    let nList = [];
-    for (let i = 0; i < str.length; i++) {
-        nList.push(str[i]);
-    }
-    return parseInt(nList.sort(function(a, b) {return b - a;}).join(''));
+  let str = String(n)
+  let nList = []
+  for (let i = 0; i < str.length; i++) {
+    nList.push(str[i])
+  }
+  return parseInt(
+    nList
+      .sort(function (a, b) {
+        return b - a
+      })
+      .join('')
+  )
 }
-
 
 // 두 번째 풀었는 때
 
-
 function solution(n) {
-    let arr = [];
-    for (let i = 0; i < String(n).length; i++) {
-        arr.push(parseInt(String(n)[i]));
-    }
-    return Number(arr.sort(function(a, b) {return b - a}).join(''));
+  let arr = []
+  for (let i = 0; i < String(n).length; i++) {
+    arr.push(parseInt(String(n)[i]))
+  }
+  return Number(
+    arr
+      .sort(function (a, b) {
+        return b - a
+      })
+      .join('')
+  )
 }
-
 
 // 빈 배열 arr = [];
 // 정수 n을 문자열로 만든 후
